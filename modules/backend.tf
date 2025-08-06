116253b901f1d1405d016e7346d9d6db08456702
Deepak Kumar <61732749+deepakkr57118@users.noreply.github.com>
2025-08-06 12:51:58 +0530

    Update backend.tf

--- modules/backend.tf
+++ modules/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraformstatedeepak"
-    key = "s3://terraformstatedeepak/terrafromstatefile/"
+    key = "s3://terraformstatedeepak/terrafromstatefile"
     region = "us-east-1"
     dynamodb_table = "terraform-table-lock"
     encrypt = true

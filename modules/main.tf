702744c46890d1c8b45a589addfbf8dff5746d83
Deepak Kumar <61732749+deepakkr57118@users.noreply.github.com>
2025-08-06 13:20:37 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -3,7 +3,9 @@ module "vpc" {
 }
 
 module "ec2" {
-  source = "./ec2"
-  subnet_id = module.vpc.public_subnet_id
-  security_group = module.vpc.security_group_id
+  source = "./modules/ec2"
+
+  security_group_id = module.vpc.security_group_id  
+  instance_type     = "t2.micro"                    
+  key_name          = "Deep-Kumar"              
 }
